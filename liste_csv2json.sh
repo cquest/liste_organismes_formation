f7370bf63cc9642d6cffedd6ffa192d24f1ca8cd
cquest <cquest@openstreetmap.fr>
2016-11-19 15:22:50 +0100

    ajout géocodage

--- liste_csv2json.sh
+++ liste_csv2json.sh
@@ -1,2 +1,6 @@
-cat ListeOF.csv | csvjson | jq '.[]|{name: .da_raison_sociale, id: .numero_de_da, context: (.numero_de_da + ", "+.da_siren+", "+(.da_siren+.da_no_etab)+", "+.adr_code_postal_postale[:2]), da_raison_sociale, da_siren, da_no_etab, adr_rue_physique, adr_rue_complement_physique, postcode: .adr_code_postal_physique, city: .adr_ville_physique, adr_rue_postale, adr_rue_complement_postale, adr_code_postal_postale, adr_ville_postale, lat: 0, lon: 0}' -c > liste_of.json
+# géocodage de la liste avec addok local (sur port 7979)
+curl http://localhost:7979/search/csv/ -F data=@ListeOF.csv -F columns=adr_rue_physique -F columns=adr_code_postal_physique -F columns=adr_ville_physique > ListeOF-geo.csv
+
+# transformation CSV en json pour addok
+cat ListeOF-geo.csv | csvjson | jq '.[]|{name: .da_raison_sociale, id: .numero_de_da, context: (.numero_de_da + ", "+.da_siren+", "+(.da_siren+.da_no_etab)+", "+.adr_code_postal_postale[:2]), da_raison_sociale, da_siren, da_no_etab, adr_rue_physique, adr_rue_complement_physique, postcode: .adr_code_postal_physique, city: .adr_ville_physique, adr_rue_postale, adr_rue_complement_postale, adr_code_postal_postale, adr_ville_postale, lat: .latitude, lon: .longitude, citycode: .result_citycode, geo_score: .result_score, geo_type: .result_type}' -c > liste_of.json
 
